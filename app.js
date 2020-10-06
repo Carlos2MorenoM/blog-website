29e7ac18204f849ab19cd5eee4d9572577130021
Carlos <c.morenomontesdeoca@gmail.com>
2020-10-06 07:15:41 +0200

    mongo DB

--- app.js
+++ app.js
@@ -32,20 +32,59 @@ const postSchema = {
 
 
 
-
  //Home Route
- app.get("/",function(req,res){
-   res.render('home', {
-     homeStartingContent: homeStartingContent,
-     posts: posts
-   })
- })
+ app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
 
 //Compose Route
 app.get("/compose",function(req,res){
   res.render('compose',)
 });
 
+app.post("/compose", function(req,res){
+
+  const post = new Post ({
+
+    title: req.body.postTitle,
+ 
+    content: req.body.postBody
+ 
+  });
+ 
+ 
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+app.get("/posts/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
+  
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+  
+  });
+  
+  app.get("/about", function(req, res){
+    res.render("about", {aboutContent: aboutContent});
+  });
+  
+  app.get("/contact", function(req, res){
+    res.render("contact", {contactContent: contactContent});
+  });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
